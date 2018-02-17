7822d4a7ab5581920d8136877df87187ef449b32
jaro6946 <jaro6946@colorado.edu>
2018-02-16 18:10:40 -0700

    see last commit

--- GPSfunctions.py
+++ GPSfunctions.py
@@ -1,28 +1,124 @@
 import numpy as np
 import cv2
-import sys
 from picamera.array import PiRGBArray #https://picamera.readthedocs.io/en/release-1.13/
-#This sintax (from ... import...) only imports a single class/fuction from the module.  PiRGB in this case.
 from picamera import PiCamera
 import time
 
 
 
-def image()
-camera = PiCamera()
-rawCapture = PiRGBArray(camera)
- 
-# allow the camera to warmup
-time.sleep(0.1)
- 
-# grab an image from the camera 
-camera.capture(rawCapture, format="bgr")
-img = rawCapture.array
+def image():
+	camera = PiCamera()
+	rawCapture = PiRGBArray(camera)
+	 
+	# allow the camera to warmup
+	time.sleep(0.1)
+	 
+	# grab an image from the camera 
+	camera.capture(rawCapture, format="bgr")
+	img = rawCapture.array
+	img=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+	#make sure the image is facing the right direction
+	#img=cv2.flip(img,0)
+
+	return img
 
 
-#make sure the image is facing the right direction
-#img=cv2.flip(img,0)
+def track(src,avgLightColor):
+	
+	def nothing(*arg):
+		pass
 
-return img
+	def limit(inputVal,limits):
+		output=inputVal
+		for i,val in enumerate(inputVal,0):
+			if val>limits[i][1]:
+				val=limits[i][1]
+			elif val<limits[i][0]:
+				val=limits[i][0]
+			output[i]=val
+
+		return output
+	
+
+	img=src.copy()	
+	thrs=50
+	filt=39
+	output=np.zeros(2,len(avgLightColor))
+
+	for j in range(len(avgLightColor)):
+		
+		upperBound=limit(avgLightColor[i]+thrs/2,[[0,179],[0,255],[0,255]])
+		lowerBound=limit(avgLightColor[i]-thrs/2,[[0,179],[0,255],[0,255]])
+		mask=np.uint8(cv2.inRange(img,lowerBound,upperBound))
+
+
+		vis = np.uint8(img.copy())
+		vis[mask==0]=(0,0,0)
+		
+		
+		gray2 = img[:,:,2] #only want black and white image
+		gray = vis[:,:,2]
+
+		blurred = cv2.GaussianBlur(gray, (filt, filt), 0)
+
+		thresholdValue = cv2.getTrackbarPos('filterThresh', 'image')
+		thresh = cv2.threshold(blurred, thresholdValue, 255, cv2.THRESH_BINARY)[1]
+		testArray=[(lower-thrs/2).tolist(),(lower+thrs/2).tolist(),lowerBound.tolist(),upperBound.tolist(),thresholdValue]
+
+
+		cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]
+	
+
+		areas=int(len(cnts))
+		splotch = np.zeros((1,areas),dtype=np.uint8)
+		
+		# loop over the contours
+		try:	
+			for i,c in enumerate(cnts,0):
+			
+				M = cv2.moments(c)
+				splotch[0][i] = int(M["m00"])
+			
+			if len(splotch)>0:
+				max1=np.argmax(splotch)
+			else:
+				max1=-1
+			
+			original=vis.copy()
+			
+			if max1>0:
+				M = cv2.moments(cnts[max1])
+				cX = int(M["m10"] / M["m00"])
+				cY = int(M["m01"] / M["m00"])
+				output[0][j]=cX
+				output[1][j]=cY
+
+				
+				cv2.drawContours(vis, [cnts[max1]], -1, (0, 255, 0), 2)
+				cv2.circle(vis, (cX, cY), 7, (255, 255, 255), -1)
+				cv2.putText(vis, i, (cX - 20, cY - 20),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+		except:
+			raise
+		
+		
+
+	visBGR=cv2.cvtColor(vis, cv2.COLOR_HSV2BGR) 
+	thresh = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+	
+	
+	
+	ch=cv2.waitKey(1)
+
+	if ch == 27:
+		exitNow=True
+		break
+
+	return output, vis
+
+
+
+
 
 
--- OpenCV_GPS.py
+++ OpenCV_GPS.py
@@ -4,6 +4,18 @@ import cv2
 import time
 
 
+class timer(object):
+	def __init__(self, timerLength):
+		self.initTime=time.time()
+		self.timerLength=float(timerLength)
+	def GO(self):
+		self.currentTime=time.time()-self.initTime
+
+
+
+
+
+
 while True:
 	
 	myNumber = str(input("Press RETURN to take initial image"))
@@ -37,42 +49,52 @@ while True:
 
 
 diff = cv2.absdiff(initImage, comparisonImage)
-mask = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
 
-th = 1
-imask =  mask>th
+# th = 1
+# imask =  mask>th
 
-canvas = np.zeros_like(img2, np.uint8)
-canvas[imask] = img2[imask]
+# canvas = np.zeros_like(img2, np.uint8)
+# canvas[imask] = img2[imask]
 
 
-cnts = cv2.findContours(imask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1][0:carCount]
+cnts = cv2.findContours(diff.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1][0:carCount]
 
 
 carCount
 contourSize= np.zeros((1,carCount),dtype=np.uint8)
 coordinates=[contourSize,contourSize]
+avgLightColor=contourSize
 # loop over the contours
 
+comparisonImage2=comparisonImage.copy()
 for i,c in enumerate(cnts,0):
 
+	lightLocation=np.zeros_like(canvas)
 	M = cv2.moments(cnts[i])
-	coordinates[0][i]= int(M["m10"] / M["m00"])
-	coordinates[1][i] = int(M["m01"] / M["m00"])
+	coordinates[0][i]= int(M["m10"] / M["m00"])#x values
+	coordinates[1][i] = int(M["m01"] / M["m00"])#y values
 
 
 	
-	cv2.drawContours(vis, [c], -1, (0, 255, 0), 2)
-	cv2.circle(vis, (cX, cY), 7, (255, 255, 255), -1)
+	cv2.drawContours(lightLocation, [c], -1, 255, -1)
+
+	avgLightColor[i]=mean(comparisonImage,lightLocation)
+	print(avgLightColor)
+
+
+while True:
+
+
+	if GO is True:
+		image=GPSfunctions.image()
+
+
+		locations, outputImg=GPSfunctions.track(image,avgLightColor)
+
+		cv2.imshow('image',np.hstack([outputImg])) 
+
 	
-	cv2.putText(vis, i, (cX - 20, cY - 20),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-
-
-
-
-
-
 
 
 
